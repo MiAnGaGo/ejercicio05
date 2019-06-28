9d563df384ea9a7874d4186277f5cfbad52e06c9
MiguelGarcia <miangarciago@ittepic.edu.mx>
2019-06-27 19:14:57 -0600

    commit tarea 5 crud

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ var user = new User({
     email: 'john@smith.io'
 });
 
+var user2 = new User({
+    name:'Miguel Garcia',
+    email:'miangarciago@ittepic.edu.mx'
+});
+
+var user3 = new User({
+    name:'Juan perez',
+    email:'juan@perez.com'
+});
+
 user.save(function(error){
     if (error){
         console.log(error);
@@ -17,4 +27,63 @@ user.save(function(error){
     }
     console.log("Saved!!");
     process.emit(0);
+});
+
+user2.save(function(error){
+    if (error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Saved!!");
+    process.emit(0);
+});
+
+user3.save(function(error){
+    if (error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Saved!!");
+    process.emit(0);
+});
+
+
+//busqueda general
+User.find({},function(error,docs){
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("consulata general");
+    console.log(docs);
+});
+
+//busqueda con restriccion
+User.find({email:'miangarciago@ittepic.edu.mx'},
+function(error,docs){
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("consulata con restriccion");
+    console.log(docs);
+});
+
+User.update({_id: '5d1563add25de13280b1db6b'},{$set: {email:'secambio@ittepic.edu'}},
+function(error,docs){
+    if(error){
+        console.log(error);
+        process.emit(1);
+    }
+    console.log("Actualizacion");
+    process.exit(0);
+});
+
+User.findByIdAndRemove({_id: '5d1563add25de13280b1db6b'},function(error,docs){
+    if(error){
+        console.log(error);
+        process.emit(1);
+    }
+    console.log(docs);
+    process.exit(0);
 });
